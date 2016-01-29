ce8929c9c71356b39b007a2547edd8da8dd417d1
Derek.Browne <korvanica@gmail.com>
2016-01-29 20:37:57 +0000

    Test Commit Derek - Added Folders

---
+++ FatPigeon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            print("w was pressed");
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+            print("a was pressed");
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            print("s was pressed");
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            print("d was pressed");
+        }
+    }
+
+}
