340e1674c8d60d5f8fdbe2b5dde1912ef3a9ee6b
Derek.Browne <korvanica@gmail.com>
2016-01-30 16:29:12 +0000

    GameController Prefab

--- FatPigeon/Assets/Scripts/ScrollingBackground.cs
+++ FatPigeon/Assets/Scripts/ScrollingBackground.cs
@@ -17,8 +17,8 @@ public class ScrollingBackground : MonoBehaviour
     // Update is called once per frame
     void Update ()
 	{
-		// Create a Vector2 offset & attach to the renderer to scroll the background.
-		Vector2 offset = new Vector2 (Time.time * gameController.obstacleMoveSpeed, 0);
+        // Create a Vector2 offset & attach to the renderer to scroll the background.
+        Vector2 offset = new Vector2 (Time.time * gameController.obstacleMoveSpeed, 0);
 		GetComponent<Renderer> ().material.mainTextureOffset = offset;
 	}
 }
