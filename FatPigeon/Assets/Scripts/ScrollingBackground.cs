45e21d8be8bbfb84065c149e5f2e840cf3e05a31
Derek.Browne <korvanica@gmail.com>
2016-01-30 14:27:55 +0000

    Background Added and Logic Changed to take speed form game controller

--- FatPigeon/Assets/Scripts/ScrollingBackground.cs
+++ FatPigeon/Assets/Scripts/ScrollingBackground.cs
@@ -6,20 +6,19 @@ using System.Collections;
 /// </summary>
 public class ScrollingBackground : MonoBehaviour
 {
-	// Background scrolling speed, can be overwritten in-engine
-	public float speed = 0.5f;
+    public GameController gameController;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
 	{
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
 
-	}
-
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update ()
 	{
 		// Create a Vector2 offset & attach to the renderer to scroll the background.
-		Vector2 offset = new Vector2 (Time.time * speed, 0);
+		Vector2 offset = new Vector2 (Time.time * gameController.obstacleMoveSpeed, 0);
 		GetComponent<Renderer> ().material.mainTextureOffset = offset;
 	}
 }
