7cdd3b50f3e156682197c96a51f7aeaa7f1ef683
Attila PAPP <darkplant@gmail.com>
2016-01-30 22:14:41 +0000

    Handling the "Object you try to Instantiate doesn't exist" error. Some comments.

--- FatPigeon/Assets/Scripts/CatControler.cs
+++ FatPigeon/Assets/Scripts/CatControler.cs
@@ -24,6 +24,10 @@ public class CatController : MonoBehaviour {
     {
     }
 
+    /// <summary>
+    /// Sets the gameobject's start position
+    /// </summary>
+    /// <param name="newPosition"></param>
     void SetStartPosition(Vector3 newPosition)
     {
         transform.position = newPosition;
